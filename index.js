ed64866e0d5d84c4a5c47edb08210991a2995b68
farzana-yesmin <farzana_cse15@yahoo.com>
2023-03-02 23:51:42 +0600

    Action Create
    -- An action is an object that has a type property

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Bismillahir Rahmanir Rahim');
+const BUY_CAKE = 'BUY_CAKE'; // indicate type of the action
+// Action is an object that has a type property
+
+// {
+//     type: BUY_CAKE,
+//     info: 'First Redux Action'
+// }
+//----------------------------------------------
+//action creator simply create an action
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First Redux Action'
+    }
+}
