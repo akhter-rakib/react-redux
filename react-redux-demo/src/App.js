0ea90fcb6bd3678c18a7dd891a584c9fa05ee5cc
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-03-06 15:31:12 +0600

    react-redux introduce

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CakeContainer from './component/CakeContainer';
+
+function App() {
+  return (
+    <div className="App">
+     <CakeContainer></CakeContainer>
+    </div>
+  );
+}
+
+export default App;
